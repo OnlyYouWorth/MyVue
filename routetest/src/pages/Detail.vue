d9ab42831e18d1c1feeace5c0330bc579b354caa
YouXiao Chen <iocodermail@gmail.com>
2023-10-07 23:29:19 +0800

    t2

---
+++ routetest/src/pages/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Detail</h3>
+    <ul>
+      <li>id: {{$route.query.id}}</li>
+      <li>content: details of {{$route.query.content}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  mounted() {
+    console.log(this.$route)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ routetest/src/pages/Message.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messageArr" :key="m.id">
+<!--        <router-link to="/home/message/detail">{{ m.content }}</router-link>&nbsp;&nbsp;-->
+        <router-link :to="{
+                       path: '/home/message/detail',
+                       query: {
+                         id: m.id,
+                         content: m.content
+                       }
+                     }">{{ m.content }}</router-link>
+      </li>
+    </ul>
+    <hr/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageArr: [
+        {id: 1, content: "message001"},
+        {id: 2, content: "message002"},
+        {id: 3, content: "message003"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
